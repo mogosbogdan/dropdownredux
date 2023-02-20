ad027a6f8b0e0ef8ab410e25004ae8e7c445630f
mogosbogdan <38261124+mogosbogdan@users.noreply.github.com>
2023-02-21 01:29:48 +0200

    css

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import DropdownMenu from "./components/DropdownMenu";
 import Card from "./components/Card";
+import "./App.css";
 
 function App() {
   return (
     <Provider store={store}>
-      <div>
+      <div className="App top">
         <DropdownMenu />
         <Card />
       </div>
