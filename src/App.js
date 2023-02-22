4ad0867426979e561041f8d7ac56612ed5ad8001
mogosbogdan <38261124+mogosbogdan@users.noreply.github.com>
2023-02-22 21:37:01 +0200

    moved provider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from "react";
-import { Provider } from "react-redux";
-import store from "./redux/store";
 import DropdownMenu from "./components/DropdownMenu";
 import Card from "./components/Card";
 import "./App.css";
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App top">
-        <DropdownMenu />
-        <Card />
-      </div>
-    </Provider>
+    <div className="App top">
+      <DropdownMenu />
+      <Card />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import store from "./redux/store";
 import "./index.css";
 
 const container = document.getElementById("root");
@@ -8,6 +10,8 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
